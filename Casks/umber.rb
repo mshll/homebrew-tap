92c5d8c5f7b9aa4ad9ef8039bb32ce72e020f315
meshal <imeshalfm@gmail.com>
2026-09-23 21:15:36 +0300

    Umber 0.2.0

--- Casks/umber.rb
+++ Casks/umber.rb
@@ -1,6 +1,6 @@
 cask "umber" do
-  version "0.1.0,10"
-  sha256 "0290f5ed638abb1a91fb44131be0015565de6e036116ebf8ba0b9ecfb0814708"
+  version "0.2.0,11"
+  sha256 "cc82f8a53a15592ebc9062a5a88f3aae7d91d08577d61ed223cb0521587d9f21"
 
   url "https://github.com/mshll/umber/releases/download/v#{version.csv.first}/Umber.dmg"
   name "Umber"
